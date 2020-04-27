0bdeb68e34b54b5ba40ddbf52579b790cee26b74
Max Brosnahan <maximilianbrosnahan@gmail.com>
2020-04-27 14:27:39 +0200

    Implement generate command

--- lib/victor/cli/commands/generate.rb
+++ lib/victor/cli/commands/generate.rb
@@ -6,14 +6,16 @@ module Victor
 
         usage "victor generate SVG_FILE [RUBY_FILE]"
         usage "victor generate (-h|--help)"
-        
+
         param "SVG_FILE", "Input SVG file"
         param "RUBY_FILE", "Output Ruby file"
 
         example "victor generate example.svg example.rb"
 
         def run
-          p args
+          svg_file = File.read(args["SVG_FILE"])
+          svg_tree = Parser.new(svg_file).parse
+          puts CodeGenerator.new(svg_tree).generate
         end
       end
     end
--- spec/victor-cli/commands/generate_spec.rb
+++ spec/victor-cli/commands/generate_spec.rb
@@ -16,6 +16,28 @@ describe 'victor generate' do
   end
 
   context "with required arguments" do
-    # Implement
+    let(:file_path) {
+      File.join(__dir__, "..", "..", "fixtures", "pacman.svg")
+    }
+
+    it "converts the svg into ruby code" do
+      code = <<~RUBY
+        require "victor"
+
+        svg = Victor::SVG.new({"width"=>"140", "height"=>"100", "style"=>"background:#ddd"})
+        svg.build do
+          rect({"x"=>"10", "y"=>"10", "width"=>"120", "height"=>"80", "rx"=>"10", "fill"=>"#666"})
+        circle({"cx"=>"50", "cy"=>"50", "r"=>"30", "fill"=>"yellow"})
+        circle({"cx"=>"58", "cy"=>"32", "r"=>"4", "fill"=>"black"})
+        polygon({"points"=>"45,50 80,30 80,70", "fill"=>"#666"})
+        circle({"cx"=>"80", "cy"=>"50", "r"=>"4", "fill"=>"yellow"})
+        circle({"cx"=>"98", "cy"=>"50", "r"=>"4", "fill"=>"yellow"})
+        circle({"cx"=>"116", "cy"=>"50", "r"=>"4", "fill"=>"yellow"})
+        end
+
+        svg.save "generated"
+      RUBY
+      expect { subject.run ["generate", file_path] }.to output(code).to_stdout
+    end
   end
 end
