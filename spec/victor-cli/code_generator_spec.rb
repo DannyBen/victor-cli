10ce350424f8e7cef49f8676524ee43e6f5055d3
Danny Ben Shitrit <db@dannyben.com>
2020-04-27 15:50:51 +0000

    refactor code_generator specs with approvals

--- spec/victor-cli/code_generator_spec.rb
+++ spec/victor-cli/code_generator_spec.rb
@@ -3,37 +3,22 @@ require "spec_helper"
 describe CodeGenerator do
   subject { described_class.new svg_tree }
 
-  describe "#generate" do
+  describe "#generate", :focus do
     let(:svg_tree) {
       ["svg", { "a" => "b" }, [["rect", { "x" => "10" }, []]]]
     }
 
     it "converts the svg tree into ruby code" do
-      expected = <<~RUBY
-        require "victor"
-
-        svg = Victor::SVG.new a: "b"
-        svg.build do
-          rect x: "10"
-        end
-
-        svg.save "generated"
-      RUBY
-      expect(subject.generate).to eql(expected)
+      expect(subject.generate).to match_fixture('code_generator/basic')
     end
 
-    context "nested nodes are present" do
+    context "with nested nodes" do
       let(:svg_tree) {
         ["g", { "a" => "b" }, [["rect", { "x" => "10" }, []]]]
       }
 
       it "converts the svg tree into ruby code" do
-        expected = <<~RUBY
-          g a: "b" do
-            rect x: "10"
-          end
-        RUBY
-        expect(subject.generate).to eql(expected)
+        expect(subject.generate).to match_fixture('code_generator/nested-nodes')
       end
     end
   end
