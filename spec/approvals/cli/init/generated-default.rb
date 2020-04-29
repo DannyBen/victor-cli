c6fff334c4678fc849c93e38ee4ac9b9d398f7ca
Danny Ben Shitrit <db@dannyben.com>
2020-04-29 19:11:30 +0000

    complete coverage

---
+++ spec/approvals/cli/init/generated-default.rb
@@ -0,0 +1,15 @@
+# Render this by running:
+# victor to-svg "spec/tmp/ghost.rb"
+
+setup viewBox: "0 0 100 100"
+
+build do
+  circle cx: 50, cy: 50, r: 40, fill: 'yellow'
+  rect x: 10, y: 50, width: 80, height: 50, fill: 'yellow'
+
+  [25, 50].each do |y|
+    circle cx: y, cy: 40, r: 8, fill: 'white'
+  end
+
+  path d: "M11 100 l13 -15 l13 15 l13 -15 l13 15 l13 -15 l13 15 Z", fill: 'white'
+end
---
+++ spec/victor-cli/commands/init_spec.rb
@@ -0,0 +1,49 @@
+require "spec_helper"
+
+describe "victor init" do
+  subject { CommandLine.router }
+
+  context "without arguments" do
+    it "shows short usage" do
+      expect { subject.run %w[init] }
+        .to output_fixture("cli/init/usage")
+    end
+  end
+
+  context "with --help" do
+    it "shows long usage" do
+      expect { subject.run %w[init --help] }
+        .to output_fixture("cli/init/help")
+    end
+  end
+
+  context "with RUBY_FILE" do
+    let(:ruby_file) { 'spec/tmp/ghost.rb' }
+    before { File.unlink ruby_file if File.exist? ruby_file }
+
+    it "saves a sample" do
+      expect { subject.run %W[init #{ruby_file}] }
+        .to output_fixture('cli/init/save')
+
+      expect(File.read ruby_file)
+        .to match_fixture('cli/init/generated-default.rb')
+    end
+
+    context "with --template NAME" do
+      it "saves using the requested template" do
+        expect { subject.run %W[init #{ruby_file} --template standalone] }
+          .to output_fixture('cli/init/save-standalone')
+        
+        expect(File.read ruby_file)
+          .to match_fixture('cli/init/generated-standalone.rb')
+      end
+
+      context "when the template is invalid" do
+        it "raises an error" do
+          expect { subject.run %W[init #{ruby_file} --template not-found] }
+            .to raise_fixture('cli/init/invalid-template')
+        end
+      end
+    end
+  end
+end
