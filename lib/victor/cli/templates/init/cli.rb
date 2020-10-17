c21375c1162827fcb33b262102c64a99519b05c4
Danny Ben Shitrit <db@dannyben.com>
2020-10-17 08:42:30 +0000

    minor template correction

--- lib/victor/cli/templates/init/cli.rb
+++ lib/victor/cli/templates/init/cli.rb
@@ -7,8 +7,8 @@ build do
   circle cx: 50, cy: 50, r: 40, fill: 'yellow'
   rect x: 10, y: 50, width: 80, height: 50, fill: 'yellow'
 
-  [25, 50].each do |y|
-    circle cx: y, cy: 40, r: 8, fill: 'white'
+  [25, 50].each do |x|
+    circle cx: x, cy: 40, r: 8, fill: 'white'
   end
 
   path d: "M11 100 l13 -15 l13 15 l13 -15 l13 15 l13 -15 l13 15 Z", fill: 'white'
