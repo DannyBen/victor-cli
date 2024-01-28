e749d2d74d8ddbbabc4065e6504ebfa669e8efb8
Danny Ben Shitrit <db@dannyben.com>
2024-01-28 14:34:33 +0000

    test with ruby 3.3

--- lib/victor/cli.rb
+++ lib/victor/cli.rb
@@ -5,8 +5,3 @@ require_relative 'cli/ruby_source'
 require_relative 'cli/css_data'
 require_relative 'cli/command_line'
 require_relative 'cli/svg_node'
-
-if ENV['BYEBUG']
-  require 'byebug'
-  require 'lp'
-end
